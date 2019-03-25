334fa3320bb0bcc0a186f5453818ed46453676e7
vivekjcsingh <Vivekjcsingh8@gmail.com>
2019-03-25 15:34:37 +0530

    Start

---
+++ addressbookapplication/home.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="addressbookapplication.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+        <tr>
+            <td>Address ID :</td>
+            <td><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td>First Name :</td>
+            <td>
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></td>
+        </tr>
+         <tr>
+            <td>Last Name</td>
+            <td>
+                <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></td>
+        </tr>
+         <tr>
+            <td>Email</td>
+            <td>
+                <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox></td>
+        </tr>
+         <tr>
+            <td>Phone Number</td>
+            <td>
+                <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td><asp:Button ID="Button1" runat="server" Text="insert" OnClick="Button1_Click" /></td>
+            <td>
+                <asp:Button ID="Button2" runat="server" Text="delete" OnClick="Button2_Click" /> <asp:Button ID="Button3" runat="server" Text="search" OnClick="Button3_Click" /><asp:Button ID="Button4" runat="server" Text="update" OnClick="Button4_Click" /></td>
+        </tr>
+    </table>
+        <h1>find the person by name</h1>
+        <table>
+             <tr>
+            <td>Last name</td>
+            <td>
+                <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox></td>
+        </tr>
+        </table>
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ addressbookapplication/home.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace addressbookapplication
+{
+    public partial class home : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand com;
+        SqlDataReader dr;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+            con = new SqlConnection("Data Source=.;Initial Catalog=addressbook;Integrated Security=True");
+            com = new SqlCommand();
+            com.Connection = con;
+            com.CommandText = "insertdata";
+            com.CommandType = System.Data.CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@firstname", TextBox2.Text);
+            com.Parameters.AddWithValue("@lastname", TextBox3.Text);
+            com.Parameters.AddWithValue("@email", TextBox4.Text);
+            com.Parameters.AddWithValue("@phone",TextBox5.Text);
+            con.Open();
+            flag=com.ExecuteNonQuery();
+            if (flag > 0)
+            {
+                Label1.Text = "data saved";
+            }
+            else
+            {
+                Label1.Text = "data not saved";
+            }
+            con.Close();
+            
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+            con = new SqlConnection("Data Source=.;Initial Catalog=addressbook;Integrated Security=True");
+            com = new SqlCommand();
+            com.Connection = con;
+            com.CommandText = "deletedata";
+            com.CommandType = System.Data.CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@id", Convert.ToInt32(TextBox1.Text));
+            con.Open();
+            flag = com.ExecuteNonQuery();
+            if (flag > 0)
+            {
+                Label1.Text = "data deleted";
+            }
+            else
+            {
+                Label1.Text = "data not deleted";
+            }
+            con.Close();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source=.;Initial Catalog=addressbook;Integrated Security=True");
+            com = new SqlCommand();
+            com.Connection = con;
+            com.CommandText = "select * from addbook where id=" +Convert.ToInt32(TextBox1.Text) + "";
+            con.Open();
+            dr = com.ExecuteReader();
+            con.Close();
+            TextBox2.Text = dr[1].ToString();
+
+            TextBox3.Text = dr[2].ToString();
+            TextBox4.Text = dr[3].ToString();
+            TextBox5.Text = dr[4].ToString();
+            
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+            con = new SqlConnection("Data Source=.;Initial Catalog=addressbook;Integrated Security=True");
+            com = new SqlCommand();
+            com.Connection = con;
+            com.CommandText = ("update addbook set firstname='" + TextBox2.Text + "',lastname='" + TextBox3.Text + "',email='" + TextBox4.Text + "',phone='" + TextBox5.Text + "' where id=" + Convert.ToInt32(TextBox1.Text) + ")");
+            con.Open();
+            flag= com.ExecuteNonQuery();
+            if (flag > 0)
+            {
+                Label1.Text = "data updated";
+            }
+            else
+            {
+                Label1.Text = "data not updated";
+            }
+            con.Close();
+        }
+
+    }
+    }
